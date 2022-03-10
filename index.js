989e058a32674c12d240009f6adbb9c853be0b9e
MMartina020 <marcsikaa15@gmail.com>
2022-03-10 13:15:34 +0100

    3

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ const config = require('./config');
 const {Sequelize} = require('sequalize');
 
 const db = new Sequelize(config.database, config.user, config.password);
+    host: config.localhost, 
+    port: config.port, 
+    dialect: "mariadb"
